e5a36cc01efb0d1bd97d255a5632b9f4fbdc36b3
gabbs28 <gabbsruiz@outlook.com>
2024-04-13 18:35:39 -0700

    fix routes

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { requireAuth } = require('../../utils/auth');
-const { Review } = require('../../db/models');
+const { User, Spot, Review, ReviewImage } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -23,34 +23,118 @@ const validateReview = [
 
 //routes go here
 
-
 //Returns all the reviews written by the current user.
-router.get("/current", requireAuth, async(req, res, next) => {
+router.get("/current", requireAuth, async(req, res, _next) => {
   const reviews = (await Review.findAll({
     where: {
-      ownerId : req.user.id
-    }
-  })).map(spot => spot.toJSON());
+      userId : req.user.id
+    },
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'firstName', 'lastName']
+      },
+      {
+        model: Spot,
+        attributes: {
+          exclude: ['description', 'createdAt', 'updatedAt']
+        }
+      },
+      {
+        model: ReviewImage,
+        attributes: ['id', 'url']
+      }
+    ]
+  }));
 
-  for(const spot of spots) {
-
-    await addSpotAverageRating(spot, "avgRating"); 
-    //awaiting it bc it is an async func
-    await addSpotPreviewImage(spot);
-  };
-
-  return res.json({Spots: spots});
+  return res.json({Reviews: reviews});
 })
 
 //Get all Reviews by a Spot's id: Returns all the reviews that belong to a spot specified by id.
-
+//See spots route
 
 //Create a Review for a Spot based on the Spot's id: Create and return a new review for a spot specified by id.
+//See spots route
 
 //Add an Image to a Review based on the Review's id: Create and return a new image for a review specified by id.
+router.post(':reviewId/images', requireAuth, async(req, res, _next) => {
+  //Extract id
+  const spotId = parseInt(req.params.spotId);
+
+  //Look up the review to add the image to (filter out spots not owned by the user)
+  const review = await Review.findByPk(spotId, {
+    where: {
+      ownerId: req.body.id
+    }
+  });
+
+  //If no spot is found return an error message
+  if (!review) {
+    return res.status(404).json({message: "Review couldn't be found"});
+  }
+
+  //Get count of review images
+  const count = ReviewImage.count({
+    where: {
+      spotId: spotId
+    }
+  });
+
+  //Can't have more than 10 images
+  if (count >= 10) {
+    return res.status(403).json({message: "Maximum number of images for this resource was reached"});
+  }
+
+  //Add the image to the spot
+  const image = await review.addReviewImage(req.body)
+
+  //Return the newly created image
+  return res.status(201).json(image);
+});
 
 //Edit a Review: Update and return an existing review.
+router.put("/:reviewId", requireAuth, validateReview, async(req, res, _next) => {
+  //Look up the review to be edited (filter out reviews not owned by the user)
+  const review = await Spot.findByPk(parseInt(req.params.reviewId), {
+    where: {
+      ownerId: req.body.id
+    }
+  });
+
+  //If no review is found return an error message
+  if (!review) {
+    return res.status(404).json({message: "Review couldn't be found"});
+  }
+
+  //Make sure the review will remain owned by the original user
+  req.body.ownerId = req.user.id;
+
+  //Update the review
+  await review.update(req.body);
+
+  //Return the updated review (make sure the call to update actually updates the variable)
+  return res.json(review);
+});
 
 //Delete a Review: Delete an existing review.
+router.delete("/:reviewId", requireAuth, async(req, res, _next) => {
+  //Look up the review to be deleted (filter out reviews not owned by the user)
+  const review = await Review.findByPk(parseInt(req.params.reviewId), {
+    where: {
+      ownerId: req.body.id
+    }
+  });
+
+  //If no spot is found return an error message
+  if (!review) {
+    return res.status(404).json({message: "Review couldn't be found"});
+  }
+
+  //Delete the spot
+  await review.destroy();
+
+  //Yay!
+  return res.json({message: "Successfully deleted"});
+});
 
 module.exports = router;
