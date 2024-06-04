b90675f6186d9b7005ba165269d353e18fccf0f2
gabbs28 <gabbsruiz@outlook.com>
2024-06-04 00:15:32 -0700

    almost done with phase0

---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+// frontend/src/main.jsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+// frontend/src/main.jsx
+
+// ...
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+// frontend/src/main.jsx
+
+// ...
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
